53219551e5b72a936c5b2f20b3a34508fc911743
emerihcelestin <emerih02@gmail.com>
2019-07-29 17:06:05 +0300

    Lien avec la base de données dans l'application

--- lib/controller.rb
+++ lib/controller.rb
@@ -1,4 +1,4 @@
-
+require 'gossip'
 class ApplicationController < Sinatra::Base
 	get '/' do 
 		erb :index
@@ -8,6 +8,6 @@ class ApplicationController < Sinatra::Base
 		erb :new_gossip
 	end	 
 	post '/gossips/new/' do
-		puts "Ce programme ne fait rien pour le moment, on va donc afficher un message dans le terminal"
+		Gossip.new.save
 	end
 end
